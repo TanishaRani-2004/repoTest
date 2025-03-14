1c4e0fb8ccec69c9864e1e498a31a7e4c3aa55ef
TanishaRani-2004 <tanisharanib0604@gmail.com>
2025-03-14 10:16:24 +0530

    Update helloWorld.java

--- helloWorld.java
+++ helloWorld.java
@@ -1,5 +1,6 @@
 import java.util.*
 public class helloWorld{
-public static void main(String args[]){
-System.out.println("Hello World from Remote Repo");
-}}
+  public static void main(String args[]){
+    System.out.println("Hello World from Remote Repo");
+}
+}
